a9b14bfc43b921363c9fcd163b0efb683407e554
Aleksandar Trivic <aleksandar.23@hotmail.com>
2022-02-11 15:10:15 +0100

    Added one more bucket to TF files

--- users.tf
+++ users.tf
@@ -2,9 +2,8 @@ resource "aws_iam_user" "user" {
   name = "app-user"
 }
 
-resource "aws_iam_user_policy_attachment" "attach-s3-read-only" {
+resource "aws_iam_user_policy_attachment" "attach-read-only-policies" {
   user       = aws_iam_user.user.name
   count      = "${length(var.policy_arn)}"
   policy_arn = "${var.policy_arn[count.index]}"
 }
-
