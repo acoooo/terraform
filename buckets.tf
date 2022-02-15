1806a59a8d6b6166e1469e2804668a7c8881137c
Aleksandar Trivic <aleksandar.23@hotmail.com>
2022-02-15 15:05:31 +0100

    Added upload bucket object

--- buckets.tf
+++ buckets.tf
@@ -7,3 +7,15 @@ resource "aws_s3_bucket" "main_bucket-2" {
   bucket = "acn-main-bucket-2"
   acl    = "private"
 }
+
+resource "aws_s3_bucket_object" "index-file" {
+  key        = "index"
+  bucket     = aws_s3_bucket.main_bucket-1.id
+  source     = "html/index.html"
+}
+
+resource "aws_s3_bucket_object" "test-file" {
+  key        = "test"
+  bucket     = aws_s3_bucket.main_bucket-2.id
+  source     = "html/test.html"
+}
--- variables.tf
+++ variables.tf
@@ -1,6 +1,6 @@
 variable "policy_arn" {
   description = "List of policies we need to attach to a user"
-  type        = list(any)
+  type        = list(string)
 }
 
 variable "primary_vpc_cidr" {
@@ -13,30 +13,30 @@ variable "secondary_vpc_cidr" {
 
 variable "primary_vpc_public_subnet_cidr" {
   description = "list of subnet cidr"
-  type        = list(any)
+  type        = list(string)
 }
 
 variable "primary_vpc_private_subnet_cidr" {
   description = "list of subnet cidr"
-  type        = list(any)
+  type        = list(string)
 }
 
 variable "primary_vpc_database_subnet_cidr" {
   description = "list of subnet cidr"
-  type        = list(any)
+  type        = list(string)
 }
 
 variable "secondary_vpc_public_subnet_cidr" {
   description = "list of subnet cidr"
-  type        = list(any)
+  type        = list(string)
 }
 
 variable "secondary_vpc_private_subnet_cidr" {
   description = "list of subnet cidr"
-  type        = list(any)
+  type        = list(string)
 }
 
 variable "secondary_vpc_database_subnet_cidr" {
   description = "list of subnet cidr"
-  type        = list(any)
+  type        = list(string)
 }
