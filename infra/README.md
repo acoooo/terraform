4ffd41070740d26b6d06d616b25c86d854765337
Aleksandar Trivic <aleksandar.23@hotmail.com>
2022-02-15 21:45:56 +0100

    Added README and task file

---
+++ infra/README.md
@@ -0,0 +1,9 @@
+# Terraform simple project
+
+This repo was created to complete AWS DevOps task, PDF file is attached in the repo.
+Folder named infra is where all of the Terraform files reside as well as the docker-compose file I used to run this project.
+Command structure I used to run my project is described below:
+
+> docker-compose run --rm trfm "CMD"
+
+This is the first version of the README file and is prone to changes.
--- infra/buckets.tf
+++ infra/buckets.tf
@@ -11,11 +11,11 @@ resource "aws_s3_bucket" "main_bucket-2" {
 resource "aws_s3_bucket_object" "index-file" {
   key        = "index"
   bucket     = aws_s3_bucket.main_bucket-1.id
-  source     = "html/index.html"
+  source     = "../html/index.html"
 }
 
 resource "aws_s3_bucket_object" "test-file" {
   key        = "test"
   bucket     = aws_s3_bucket.main_bucket-2.id
-  source     = "html/test.html"
+  source     = "../html/test.html"
 }
